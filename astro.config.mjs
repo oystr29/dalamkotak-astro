b29392600c2de05903b596b1a2b60556b7f7732d
oktoala <okta.yoga.syah@gmail.com>
2024-09-05 21:33:42 +0800

    test webcam

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,17 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import svelte from '@astrojs/svelte';
+import svelte from "@astrojs/svelte";
 import netlify from "@astrojs/netlify";
-
 import icon from "astro-icon";
+
+import auth from "auth-astro";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://dalamkotak.com",
-  integrations: [mdx(), sitemap(), tailwind(), svelte(), icon()],
-  output: "hybrid",
-  adapter: netlify()
+  integrations: [mdx(), sitemap(), tailwind(), svelte(), icon(), auth()],
+  output: "server",
+  adapter: netlify(),
 });
+
---
+++ src/components/svelte/webcam.svelte
@@ -0,0 +1,12 @@
+<script>
+/** @type {HTMLVideoElement | undefined}*/
+let videoEl;
+	navigator.mediaDevices.getUserMedia({video: true}).then(stream => {
+	videoEl.srcObject = stream;
+});
+
+</script>
+
+      <video bind:this={videoEl} autoplay playsinline class="w-screen h-screen object-cover absolute" id="">
+        <track kind="captions" />
+      </video>
